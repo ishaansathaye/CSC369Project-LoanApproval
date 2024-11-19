09a102e495ee64e1fa8682c5cb7d59b455f0479e
AnsonYams <ansonyamvinij@gmail.com>
2024-11-18 16:51:17 -0800

    Cleanup processData function

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -5,80 +5,70 @@ import plotly._, plotly.element._, plotly.layout._, plotly.Plotly._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-case class HealthData (heartDisease: Double, bmi: Double, smoking: Double, alcohol: Double, stroke: Double,
- physicalHealth: Double, mentalHealth: Double, diffWalking: Double, sex: Double, age: Double, race: Double,
- diabetic: Double, activity: Double, generalHealth: Double, sleep: Double, asthma: Double, kidneyDisease: Double,
- skinCancer: Double)
+case class LoanApplicant (loanType: Double, gender: Double, ownCar: Double, ownRealty: Double, income: Double, credit: Double, annuity: Double,
+ age: Double, employmentDays: Double, registrationDays: Double, education: Double, familyStatus: Double,
+ houseType: Double, numChildren: Double, relativePopulation: Double)
 
 object App {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("NameOfApp").setMaster("local[*]")
     val sc = new SparkContext(conf)
 
-    val data = sc.textFile("src/heart_2020_cleaned.csv")
+    val data = sc.textFile("src/application_train.csv")
 
     val processData = data.flatMap { line =>
-      val fields = line.split(",")
-      fields match {
-        case Array(hd, b, s, a, st, ph, mh, dw, sex, age, race, d, act, gh, sl, ast, kd, sc) =>
-          Some(HealthData(
-            if (hd == "Yes") 1.0 else 0.0,
-            b.toDouble,
-            if (s == "Yes") 1.0 else 0.0,
-            if (a == "Yes") 1.0 else 0.0,
-            if (st == "Yes") 1.0 else 0.0,
-            ph.toDouble,
-            mh.toDouble,
-            if (dw == "Yes") 1.0 else 0.0,
-            if (sex == "Male") 1.0 else 0.0,
-            age match {
-              case "18-24" => 1.0
-              case "25-29" => 2.0
-              case "30-34" => 3.0
-              case "35-39" => 4.0
-              case "40-44" => 5.0
-              case "45-49" => 6.0
-              case "50-54" => 7.0
-              case "55-59" => 8.0
-              case "60-64" => 9.0
-              case "65-69" => 10.0
-              case "70-74" => 11.0
-              case "75-79" => 12.0
-              case "80 or older" => 13.0
+    val fields = line.split(",")
+    fields match {
+      case Array(
+            loanType, gender, ownCar, ownRealty, income, credit, annuity, ageDays, employmentDays,
+            registrationDays, education, familyStatus, houseType, numChildren, relativePopulation
+          ) =>
+        Some(LoanApplicant(
+          loanType.toDouble,
+          if (gender == "F") 1.0 else 0.0,
+          ownCar.toDouble,
+          ownRealty.toDouble,
+          income.toDouble,
+          credit.toDouble,
+          annuity.toDouble,
+          ageDays.toDouble,
+          employmentDays.toDouble,
+          registrationDays.toDouble,
+          education match {
+              case "Secondary / secondary special" => 1.0
+              case "Higher education" => 2.0
+              case "Incomplete higher" => 3.0
+              case "Lower secondary" => 4.0
+              case "Academic degree" => 5.0
               case _ => 0.0
-            },
-            race match {
-              case "White" => 1.0
-              case "Black" => 2.0
-              case "American Indian/Alaskan Native" => 3.0
-              case "Asian" => 4.0
-              case "Hispanic" => 5.0
-              case "Other" => 6.0
-              case _ => 0.0
-            },
-            d match {
-              case "No, borderline diabetes" => 0.0
-              case "No" => 0.0
-              case "Yes" => 1.0
-              case "Yes (during pregnancy)" => 1.0
-            },
-            if (act == "Yes") 1.0 else 0.0,
-            gh match {
-              case "Excellent" => 1.0
-              case "Very good" => 2.0
-              case "Good" => 3.0
-              case "Fair" => 4.0
-              case "Poor" => 5.0
-              case _ => 0.0
-            },
-            sl.toDouble,
-            if (ast == "Yes") 1.0 else 0.0,
-            if (kd == "Yes") 1.0 else 0.0,
-            if (sc == "Yes") 1.0 else 0.0
-          ))
-        case _ => None
-      }
+          },
+          familyStatus match {
+            case "Single / not married" => 1.0
+            case "Married" => 2.0
+            case "Civil marriage" => 3.0
+            case "Widow" => 4.0
+            case "Separated" => 5.0
+            case "Unknown" => 6.0
+            case _ => 0.0
+          },
+          houseType match {
+            case "House / apartment" => 1.0
+            case "Rented apartment" => 2.0
+            case "With parents" => 3.0
+            case "Municipal apartment" => 4.0
+            case "Office apartment" => 5.0
+            case "Co-op apartment" => 6.0
+            case _ => 0.0
+          },
+          numChildren.toDouble,
+          relativePopulation.toDouble
+        ))
+      case _ => None
     }
+  }
+
+
+
     val clean_data = processData.map(h => h.productIterator.mkString(", "))
     clean_data.saveAsTextFile("src/output")
 
